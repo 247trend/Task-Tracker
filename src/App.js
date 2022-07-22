b74affee7f74b21da28fd3be5f69aa5bfc7ab5a3
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-22 09:42:24 +0930

    Link tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Meeting at George St", day: "Feb 6th at 2:00pm", reminder: true },
+    { id: 2, text: "Check Email", day: "Feb 8th at 10:30am", reminder: true },
+    { id: 3, text: "Shopping", day: "Feb 9th at 3:40pm", reminder: false },
+  ]);
+
   return (
     <div className="App container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,16 +1,10 @@
-import { useState } from "react";
+import Task from "./Task";
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    { id: 1, text: "Meeting at George St", day: "Feb 6th at 2:00pm", reminder: true },
-    { id: 2, text: "Check Email", day: "Feb 8th at 10:30am", reminder: true },
-    { id: 3, text: "Shopping", day: "Feb 9th at 3:40pm", reminder: false },
-  ]);
-
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task}/>
       ))}
     </>
   );
