35ff102f01e8a6c2aacfcffbcca5e905df72ab17
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-23 19:15:54 +0930

    Implement add function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
+
+  const [showAdd, setShowAdd] = useState(false);
+  
   const [tasks, setTasks] = useState([
     { id: 1, text: "Meeting at George St", day: "Feb 6th at 2:00pm", reminder: true },
     { id: 2, text: "Check Email", day: "Feb 8th at 10:30am", reminder: true },
     { id: 3, text: "Shopping", day: "Feb 9th at 3:40pm", reminder: false },
   ]);
+
+  //Add Task
+  const addTask = (task)=>{
+    const id = Math.floor(Math.random()*10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
   //Delete Task
   const deleteTask = (id) => {
@@ -21,8 +32,9 @@ function App() {
   };
 
   return (
-    <div className="App container">
-      <Header />
+    <div className="container">
+      <Header onAdd={()=> setShowAdd(!showAdd)} showAdd={showAdd}/>
+      {showAdd && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No Task To Show!"}
     </div>
   );
